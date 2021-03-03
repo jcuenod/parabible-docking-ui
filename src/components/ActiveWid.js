6e24cae09276399037584ce8ac2b86f860e2d361
James Cuénod <jcuenod@users.noreply.github.com>
2021-03-03 14:01:45 -0600

    Whitespace

--- src/components/ActiveWid.js
+++ src/components/ActiveWid.js
@@ -1,16 +1,15 @@
-
 const ActiveWid = {
-    _activeWid: -1,
-    _subscribers: [],
-    get activeWid() {
-        return this._activeWid
-    },
-    set activeWid(wid) {
-        this._activeWid = wid
-        this._subscribers.forEach(s => s(wid))
-    },
-    subscribe(subscriber) {
-        this._subscribers.push(subscriber)
-    }
+	_activeWid: -1,
+	_subscribers: [],
+	get activeWid() {
+		return this._activeWid
+	},
+	set activeWid(wid) {
+		this._activeWid = wid
+		this._subscribers.forEach(s => s(wid))
+	},
+	subscribe(subscriber) {
+		this._subscribers.push(subscriber)
+	}
 }
 export default ActiveWid
